61411f522ae659d668f3cd1315648760d0261d49
itzelll <mmitzel2435@gmail.com>
2024-10-28 22:46:45 -0600

    update

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import './pokedex.png'
+import ImgPokedex from './pokedex.png'
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+const tiposPokemones = [
+    'normal', 'fire', 'water', 'grass', 'electric', 'ice',
+    'fighting', 'poison', 'ground', 'flying', 'psychic',
+    'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'
+]; //lista tipos
 
 const ListaPokemones = () => {
     const [pokemon, setPokemon] = useState([]);
     const [carga, setCarga] = useState(true);
+    //    const [tipo, setTipo] = useState([]);
+    const [tipoSeleccionado, setTipoSeleccionado] = useState('') //tipo texto
 
     useEffect(() => {
         const fetchPokemones = async () => {
@@ -22,12 +30,12 @@ const ListaPokemones = () => {
                             name: pokeData.name,
                             weight: pokeData.weight,
                             height: pokeData.height,
-                            types: pokeData.types.map(typeInfo => typeInfo.type.name), // Obtener tipos
+                            types: pokeData.types.map(typeInfo => typeInfo.type.name), //tipos
                             image: pokeData.sprites.front_default,
                         };
                     })
                 );
-                setPokemon(data.detalles); //<--para guardar lo anterior
+                setPokemon(detalles); //<--guardar todo
                 //setPokemon(data.results);
             } catch (error) {
                 console.error('Error al cargar pokemones', error);
@@ -35,41 +43,68 @@ const ListaPokemones = () => {
                 setCarga(false);
             }
         };
-        fetchPokemones();
-    }, []);
+        fetchPokemones(); //llamar funcion
+    }, []); //carga inicial datos 1 vez
+
+    //filtro tipos de pokemones
+    const filtroPokemon = tipoSeleccionado //filtro directo
+        ? pokemon.filter( //obtener tipo seleccionado
+            (poke) =>
+                poke.types.includes(tipoSeleccionado))
+        : pokemon;
+
+    /*
+    const filtroTipos = () => {
+        const filtrarpokemon = tipoSeleccionado.filter(
+            (poke) => 
+            poke.types.includes(tipoSeleccionado));
+            setTipoSeleccionado(tipoSeleccionado);
+    } 
+
+    useEffect(() => {
+        filtroTipos();        
+    }, [])
+    */
+
+    const clicHome = () => { //borrar filtro
+        setTipoSeleccionado('');
+    }
 
     return (
-        <div className='encabezado'>
-            <main className="main min-vh-100">
-                <div className="container navbar-dark bg-dark">
-                    <nav className="navbar navbar-expand-lg">
-                        <div className="container-fluid">
-                            <div className="navbar-brand" href="#">
-                                <img src="/pokedex.png" width="250" />
-                                <div className="collapse navbar-collapse" id="navbarNav">
-                                    <div className="btn-group" role="group">
-                                        <button type="button" className="btn btn-primary" id="home">
-                                            Home
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
+        <div>
+            <main className="main">
+                <nav className="navbar">
+                    <div className="header">
+                        <img src={ImgPokedex} width="180" />
+                        <button type="button" className="header-buttons nav-button" onClick={clicHome}>
+                            Home
+                        </button>
+                        <select
+                        className='header-buttons nav-button'
+                        value={tipoSeleccionado}
+                        onChange={(e) => setTipoSeleccionado(e.target.value)}
+                    >
+                        <option value="">Tipo pokemon</option>
+                        {tiposPokemones.map((tipo) => (
+                            <option key={tipo} value={tipo}>
+                                {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+                            </option>
+                        ))}
+                    </select>
+                    </div>                    
+                </nav>
                 <br />
-                <div className="container" id="divContent">
-                    <div className="row border"></div>
+                <div>
                     <div className='pokedex'>
                         <p></p>
                         {carga ? (
                             <p>Cargando...</p>
                         ) : (
                             <div className='pokemon-list'>
-                                {pokemon.map((pokemon, index) => (
+                                {filtroPokemon.map((pokemon, index) => (
                                     <div key={index} className="pokemon-item">
                                         <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
-                                        <img src={pokemon.image} alt={pokemon.name} />
+                                        <img src={pokemon.image} alt={pokemon.name} className="img-p" />
                                         <p>Peso: {pokemon.weight} hectogramos</p>
                                         <p>Altura: {pokemon.height} decímetros</p>
                                         <p>Tipo: {pokemon.types.join(', ')}</p>
